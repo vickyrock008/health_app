0ae2ed42cbe71b78dcdf08a990f59df7d7a24c0a
vickyrock008 <115275288+vickyrock008@users.noreply.github.com>
2025-07-02 15:19:16 +0530

    app.py

--- app.py
+++ app.py
@@ -148,8 +148,8 @@ def extract_health_data():
 
 
 
-
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True, port=5500)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
